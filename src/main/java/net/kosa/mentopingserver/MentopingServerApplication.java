75e8d1e2d8380fdc19abf26f2580d39ee9b45561
onestone <onestone1380@gmail.com>
2024-09-20 14:31:47 +0900

    first commit

---
+++ src/main/java/net/kosa/mentopingserver/MentopingServerApplication.java
@@ -0,0 +1,13 @@
+package net.kosa.mentopingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentopingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentopingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/kosa/mentopingserver/MentopingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kosa.mentopingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentopingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
