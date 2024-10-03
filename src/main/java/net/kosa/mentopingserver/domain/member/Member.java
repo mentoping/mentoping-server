9b7d0de4712e49dee0fc216fbaa6583067cf2c83
onestone <onestone1380@gmail.com>
2024-10-03 17:59:14 +0900

    Feat Member, Mentor

--- src/main/java/net/kosa/mentopingserver/domain/member/Member.java
+++ src/main/java/net/kosa/mentopingserver/domain/member/Member.java
@@ -5,18 +5,19 @@ import lombok.NoArgsConstructor;
 
 import jakarta.persistence.*;
 import lombok.experimental.SuperBuilder;
+import net.kosa.mentopingserver.domain.answer.Answer;
+import net.kosa.mentopingserver.domain.post.Post;
 import net.kosa.mentopingserver.global.common.entity.BaseEntity;
 import net.kosa.mentopingserver.global.common.enums.Role;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @SuperBuilder(toBuilder = true)
 public class Member extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -40,4 +41,10 @@ public class Member extends BaseEntity {
     @Lob
     @Column(columnDefinition = "LONGTEXT")
     private String content;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Answer> answers = new ArrayList<>();
 }
