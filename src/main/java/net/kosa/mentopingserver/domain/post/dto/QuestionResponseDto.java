38fb83c35b00a1bd60a5439ddbe5284f3cb45b3d
onestone <onestone1380@gmail.com>
2024-10-06 16:20:02 +0900

    Update DTO

---
+++ src/main/java/net/kosa/mentopingserver/domain/post/dto/QuestionResponseDto.java
@@ -0,0 +1,29 @@
+package net.kosa.mentopingserver.domain.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.kosa.mentopingserver.domain.mentor.dto.AuthorDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private AuthorDto author;
+    private LocalDateTime createdAt;
+    private String category;
+    private List<String> hashtags;
+    private int likeCount;
+    private int answerCount;
+    private boolean isSelected;
+
+}
