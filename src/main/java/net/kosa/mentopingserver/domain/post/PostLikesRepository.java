2174171aa8be8fc7e005070d7cc29f54e6d5b7e0
onestone <onestone1380@gmail.com>
2024-10-06 16:23:46 +0900

    Feat PostLikeRepository

--- src/main/java/net/kosa/mentopingserver/domain/post/PostLikesRepository.java
+++ src/main/java/net/kosa/mentopingserver/domain/post/PostLikesRepository.java
@@ -1,7 +1,18 @@
 package net.kosa.mentopingserver.domain.post;
 
+import net.kosa.mentopingserver.domain.post.entity.PostLikes;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface PostLikesRepository extends JpaRepository<PostLikes, Long> {
 
+    // 특정 게시물과 회원의 좋아요 여부 확인
+    Optional<PostLikes> findByPost_IdAndMember_Id(Long postId, Long memberId);
+
+    // 게시물의 좋아요 수 확인
+    int countByPost_Id(Long postId);
+
+    // 특정 게시물과 회원의 좋아요 삭제
+    void deleteByPost_IdAndMember_Id(Long postId, Long memberId);
 }
