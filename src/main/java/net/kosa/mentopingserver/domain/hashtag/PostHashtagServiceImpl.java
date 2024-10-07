15a9f48037dfe666ddb5547119e1d02301c8d00f
onestone <onestone1380@gmail.com>
2024-10-07 21:31:16 +0900

    Update Post Hashtag Service

--- src/main/java/net/kosa/mentopingserver/domain/hashtag/PostHashtagServiceImpl.java
+++ src/main/java/net/kosa/mentopingserver/domain/hashtag/PostHashtagServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,21 +24,16 @@ public class PostHashtagServiceImpl implements PostHashtagService {
         List<PostHashtag> previousHashtags = getPostHashtags(post);
 
         // 유효한 해시태그 리스트 필터링 (빈 값 제외)
-        List<String> validHashtags = hashtagList.stream()
+        Set<String> validHashtags = hashtagList.stream()
                 .map(String::trim)
                 .filter(s -> !s.isEmpty())
-                .toList();
+                .collect(Collectors.toSet());
 
-        // 삭제할 해시태그 목록 생성 (기존에 있는데 새로운 리스트에 없는 해시태그들)
-        List<PostHashtag> needToDelete = previousHashtags.stream()
-                .filter(preHashtag -> !validHashtags.contains(preHashtag.getHashtag().getName()))
-                .collect(Collectors.toList());
+        // 기존 해시태그 모두 삭제
+        postHashtagRepository.deleteAll(previousHashtags);
 
         // 새로운 해시태그 저장
         validHashtags.forEach(hashtag -> saveHashtag(post, hashtag));
-
-        // 더 이상 포스트와 연결되지 않은 해시태그 삭제
-        postHashtagRepository.deleteAll(needToDelete);
     }
 
     @Override
@@ -48,13 +44,10 @@ public class PostHashtagServiceImpl implements PostHashtagService {
     private PostHashtag saveHashtag(Post post, String hashtagStr) {
         Hashtag hashtag = hashtagService.save(hashtagStr);
 
-        return postHashtagRepository.findByPost_IdAndHashtag_Id(post.getId(), hashtag.getId())
-                .orElseGet(() -> {
-                    PostHashtag postHashtag = PostHashtag.builder()
-                            .post(post)
-                            .hashtag(hashtag)
-                            .build();
-                    return postHashtagRepository.save(postHashtag);
-                });
+        PostHashtag postHashtag = PostHashtag.builder()
+                .post(post)
+                .hashtag(hashtag)
+                .build();
+        return postHashtagRepository.save(postHashtag);
     }
 }
