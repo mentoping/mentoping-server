187dbb2d70cba6672d9a1c12ee21ef6b8f0958c1
onestone <onestone1380@gmail.com>
2024-10-07 21:31:29 +0900

    Update Post Repository

--- src/main/java/net/kosa/mentopingserver/domain/post/PostRepository.java
+++ src/main/java/net/kosa/mentopingserver/domain/post/PostRepository.java
@@ -1,5 +1,6 @@
 package net.kosa.mentopingserver.domain.post;
 
+import net.kosa.mentopingserver.domain.member.Member;
 import net.kosa.mentopingserver.domain.post.entity.Post;
 import net.kosa.mentopingserver.global.common.enums.SubCategory;
 import org.springframework.data.domain.Page;
@@ -9,7 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+    
+    Page<Post> findByMember(Member member, Pageable pageable);
 
-    List<Post> findByCategory(SubCategory category);
-
+    Page<Post> findByCategory(SubCategory category, Pageable pageable);
 }
