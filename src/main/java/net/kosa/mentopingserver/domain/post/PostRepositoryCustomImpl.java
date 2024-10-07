54ac421a99d6e7f93e094d3de1961662195d8716
onestone <onestone1380@gmail.com>
2024-10-07 12:12:29 +0900

    Update Post Repository

--- src/main/java/net/kosa/mentopingserver/domain/post/PostRepositoryCustomImpl.java
+++ src/main/java/net/kosa/mentopingserver/domain/post/PostRepositoryCustomImpl.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import net.kosa.mentopingserver.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,5 +25,24 @@ public class PostRepositoryCustomImpl implements PostRepositoryCustom {
         query.setParameter("postId", postId);
         List<Post> results = query.getResultList();
         return results.stream().findFirst();
+    }
+
+    @Override
+    public Page<Post> findAllWithAnswers(Pageable pageable) {
+        List<Post> posts = entityManager.createQuery(
+                        "SELECT p FROM Post p LEFT JOIN FETCH p.answers",
+                        Post.class
+                )
+                .setFirstResult((int) pageable.getOffset())
+                .setMaxResults(pageable.getPageSize())
+                .getResultList();
+
+        long totalCount = getTotalCount(); // 전체 포스트의 개수를 얻는 메서드
+
+        return new PageImpl<>(posts, pageable, totalCount);
+    }
+
+    private long getTotalCount() {
+        return entityManager.createQuery("SELECT COUNT(p) FROM Post p", Long.class).getSingleResult();
     }
 }
