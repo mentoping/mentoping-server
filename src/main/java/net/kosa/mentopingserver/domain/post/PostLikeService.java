1e3663645292971d6ff2811d41beb567878049e7
onestone <onestone1380@gmail.com>
2024-10-09 14:01:34 +0900

    Feat PostLikes Function

---
+++ src/main/java/net/kosa/mentopingserver/domain/post/PostLikeService.java
@@ -0,0 +1,4 @@
+package net.kosa.mentopingserver.domain.post;
+
+public interface PostLikeService {
+}
---
+++ src/main/java/net/kosa/mentopingserver/domain/post/PostLikeServiceImpl.java
@@ -0,0 +1,55 @@
+package net.kosa.mentopingserver.domain.post;
+
+import lombok.RequiredArgsConstructor;
+import net.kosa.mentopingserver.domain.member.Member;
+import net.kosa.mentopingserver.domain.member.MemberRepository;
+import net.kosa.mentopingserver.domain.post.entity.Post;
+import net.kosa.mentopingserver.domain.post.entity.PostLikes;
+import net.kosa.mentopingserver.global.exception.MemberNotFoundException;
+import net.kosa.mentopingserver.global.exception.PostNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostLikeServiceImpl implements PostLikeService{
+
+    private final PostLikesRepository postLikesRepository;
+    private final PostRepository postRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void addLike(Long postId, Long memberId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("Post not found with id: " + postId));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException("Member not found with id: " + memberId));
+
+        if (!postLikesRepository.existsByPostAndMember(post, member)) {
+            post.incrementLikeCount();
+            postLikesRepository.save(PostLikes.builder().post(post).member(member).build());
+        }
+    }
+
+    @Transactional
+    public void removeLike(Long postId, Long memberId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("Post not found with id: " + postId));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException("Member not found with id: " + memberId));
+
+        PostLikes postLike = postLikesRepository.findByPostAndMember(post, member)
+                .orElseThrow(() -> new RuntimeException("Like not found"));
+
+        post.decrementLikeCount();
+        postLikesRepository.delete(postLike);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean hasUserLikedPost(Long postId, Long memberId) {
+        return postLikesRepository.existsByPostIdAndMemberId(postId, memberId);
+    }
+
+}
