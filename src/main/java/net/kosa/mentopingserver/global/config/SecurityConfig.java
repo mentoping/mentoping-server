d12cb5925277de621e014b3cfbb33083c1fd906b
onestone <onestone1380@gmail.com>
2024-10-10 21:39:35 +0900

    Update Security Config

--- src/main/java/net/kosa/mentopingserver/global/config/SecurityConfig.java
+++ src/main/java/net/kosa/mentopingserver/global/config/SecurityConfig.java
@@ -1,22 +1,55 @@
 package net.kosa.mentopingserver.global.config;
 
+import net.kosa.mentopingserver.domain.login.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
 
-    // 오재혁이 채팅 테스트 하기위해 임의로 시큐리티 요청 모두 허용시킴
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //csrf disable
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (필요한 경우에만)
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll() // 모든 요청에 대해 인증 없이 접근 허용
-                );
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //oauth2
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)));
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
