6218d6c0e9ddfbcbeb269aa44ed7467ce58c0fd8
onestone <onestone1380@gmail.com>
2024-10-12 00:58:04 +0900

    Update QuestionDataInsertionTest

--- src/test/java/net/kosa/mentopingserver/domain/post/QuestionDataInsertionTest.java
+++ src/test/java/net/kosa/mentopingserver/domain/post/QuestionDataInsertionTest.java
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Transactional
+//@Transactional
 //@ActiveProfiles("test")
 public class QuestionDataInsertionTest {
 
