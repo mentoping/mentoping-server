2d01b7f28de1dc77ab5731199e16325c64dc1d51
onestone <onestone1380@gmail.com>
2024-10-11 12:03:53 +0900

    Update Category name

--- src/main/java/net/kosa/mentopingserver/global/common/enums/Category.java
+++ src/main/java/net/kosa/mentopingserver/global/common/enums/Category.java
@@ -7,17 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Category {
 
-    // 프로그래밍
-    ITSW("it_sw"),
-//    DESIGN("디자인"),
+    ITSW("ITSW"),
     DESIGN("DESIGN"),
-    MARKETING("마케팅"),
-    FINANCE("금융/은행/보험"),
-    MD("MD/상품기획"),
-    ADBERTISE("광고/홍보"),
-    PRIDUCTION("생산/제조"),
-    MEDIA("미디어"),
-    ETC("기타");
+    MARKETING("MARKETING"),
+    FINANCE("FINANCE"),
+    MD("MD"),
+    ADBERTISE("ADBERTISE"),
+    PRIDUCTION("PRIDUCTION"),
+    MEDIA("MEDIA"),
+    ETC("ETC");
 
     private final String name;
 }
