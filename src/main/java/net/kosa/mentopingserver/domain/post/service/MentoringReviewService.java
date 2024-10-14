56b53401c18b20d429d6574b154f8775c13cd599
onestone <onestone1380@gmail.com>
2024-10-15 07:40:48 +0900

    Style trim

--- src/main/java/net/kosa/mentopingserver/domain/post/service/MentoringReviewService.java
+++ src/main/java/net/kosa/mentopingserver/domain/post/service/MentoringReviewService.java
@@ -1,28 +1,25 @@
 package net.kosa.mentopingserver.domain.post.service;
 
 import net.kosa.mentopingserver.domain.post.dto.MentoringReviewDto;
-import net.kosa.mentopingserver.domain.post.entity.MentoringReview;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Map;
 
 public interface MentoringReviewService {
 
     @Transactional
-    public MentoringReviewDto createReview(Long mentoringId, Long applicationId, MentoringReviewDto reviewDto, Long memberId);
+    MentoringReviewDto createReview(Long mentoringId, Long applicationId, MentoringReviewDto reviewDto, Long memberId);
 
     @Transactional(readOnly = true)
     MentoringReviewDto getReview(Long reviewId);
 
     @Transactional
-    public MentoringReviewDto updateReview(Long reviewId, MentoringReviewDto reviewDto, Long memberId);
+    MentoringReviewDto updateReview(Long reviewId, MentoringReviewDto reviewDto, Long memberId);
 
     @Transactional
-    public void deleteReview(Long reviewId, Long memberId);
+    void deleteReview(Long reviewId, Long memberId);
 
     @Transactional(readOnly = true)
     Page<MentoringReviewDto> getReviewsByMentoring(Long mentoringId, Pageable pageable);
