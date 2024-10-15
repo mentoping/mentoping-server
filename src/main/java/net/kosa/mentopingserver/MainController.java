173f4ddbae160ab7eda305eb82a6296af02d313d
onestone <onestone1380@gmail.com>
2024-10-16 06:16:39 +0900

    Feat main controller

--- src/main/java/net/kosa/mentopingserver/MainController.java
+++ src/main/java/net/kosa/mentopingserver/MainController.java
@@ -1,5 +1,9 @@
 package net.kosa.mentopingserver;
 
+import net.kosa.mentopingserver.domain.login.CustomOAuth2User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,13 @@ public class MainController {
     @GetMapping("/version")
     public String version() {
         return "Metain version 1.0.0";
+    }
+
+    @GetMapping("/user-info")
+    public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        if (customOAuth2User != null) {
+            return ResponseEntity.ok(customOAuth2User);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
     }
 }
