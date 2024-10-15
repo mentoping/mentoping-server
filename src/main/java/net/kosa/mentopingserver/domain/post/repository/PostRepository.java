142c610d73beb66626a3d5368b32337f6a6ae1c2
Jaehyuk0922 <dhwogur0@gmail.com>
2024-10-16 06:28:40 +0900

    Feat 특정 멤버 멘토링 수 조회
    특정 멤버가 멘토링 수 조회하는 로직 구현

--- src/main/java/net/kosa/mentopingserver/domain/post/repository/PostRepository.java
+++ src/main/java/net/kosa/mentopingserver/domain/post/repository/PostRepository.java
@@ -3,7 +3,6 @@ package net.kosa.mentopingserver.domain.post.repository;
 import net.kosa.mentopingserver.domain.member.entity.Member;
 import net.kosa.mentopingserver.domain.post.entity.Post;
 import net.kosa.mentopingserver.global.common.enums.Category;
-import net.kosa.mentopingserver.global.common.enums.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Map;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     
@@ -29,13 +27,15 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostRepositor
     @Query("SELECT pl.post FROM PostLikes pl WHERE pl.member = :member AND pl.post.price IS NOT NULL")
     Page<Post> findLikedMentoringsByMember(@Param("member") Member member, Pageable pageable);
 
-//    @Query("SELECT p.category, COUNT(p) FROM Post p WHERE p.category IN :categories GROUP BY p.category")
     @Query("SELECT p.category, COUNT(p) FROM Post p WHERE p.price IS NULL AND p.category IN :categories GROUP BY p.category")
     List<Object[]> countPostsByCategory(@Param("categories") List<Category> categories);
 
     @Query("SELECT p.category, COUNT(p) FROM Post p WHERE p.price IS NOT NULL AND p.category IN :categories GROUP BY p.category")
     List<Object[]> countMentoringsByCategory(@Param("categories") List<Category> categories);
 
+    // 특정 멤버가 작성한 멘토링 (price가 null이 아닌) 포스트의 수를 모두 조회하는 메서드
+    @Query("SELECT COUNT(p) FROM Post p WHERE p.member = :member AND p.price IS NOT NULL")
+    Long findAllMentoringsByMember(@Param("member") Member member);
 
 
 }
