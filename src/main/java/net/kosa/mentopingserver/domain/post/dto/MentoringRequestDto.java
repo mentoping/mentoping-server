28be5ce331cae9176feec4cb66f4134052f3bcef
Jaehyuk0922 <dhwogur0@gmail.com>
2024-10-17 10:50:44 +0900

    Update Mentoring S3 Uplload (#67)
    멘토링 썸네일 s3 업로드 작업

--- src/main/java/net/kosa/mentopingserver/domain/post/dto/MentoringRequestDto.java
+++ src/main/java/net/kosa/mentopingserver/domain/post/dto/MentoringRequestDto.java
@@ -11,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.kosa.mentopingserver.global.common.enums.Category;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 @Data
@@ -37,7 +38,7 @@ public class MentoringRequestDto {
     private Long price;
 
     @Nullable
-    private String thumbnailUrl;
+    private MultipartFile thumbnailUrl;
 
     @NotBlank(message = "요약은 필수 입력 항목입니다.")
     @Size(max = 500, message = "요약은 500자를 초과할 수 없습니다.")
